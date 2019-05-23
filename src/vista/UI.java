48694d3515eec9e6cef9e997e6033b3b9c45b2a6
Coral <Coral@DESKTOP-PRHLIFA>
2019-05-23 20:27:07 +0200

    Termina la vista AltaPaciente

--- src/vista/UI.java
+++ src/vista/UI.java
@@ -7,17 +7,22 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import vista.paciente.AltaPaciente;
+
 public class UI extends JFrame {
 
 	private JPanel contentPane;
+
+	private AltaPaciente altaPaciente = new AltaPaciente();
 
 	public UI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.add(this.altaPaciente, BorderLayout.CENTER);
 		setLocationRelativeTo(null);
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
 	}
--- src/vista/paciente/AltaPaciente.java
+++ src/vista/paciente/AltaPaciente.java
@@ -1,11 +1,108 @@
 package vista.paciente;
 
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+
+import javax.swing.border.MatteBorder;
 
 public class AltaPaciente extends JPanel {
 
-	public AltaPaciente() {
+	private String[] label = { "ID :", "Nombre :", "Apellidos :", "Direccion :", "Nacimiento :", "Telefono :" };
+	private JTextField[] fields = new JTextField[this.label.length];
+	private JPanel panelLabel = new JPanel();
+	private JPanel panelText = new JPanel();
+	private int letraPequena = 25;
 
+	public AltaPaciente() {
+		setVisible(true);
+		setBackground(Color.WHITE);
+
+		JLabel lblNewLabel = new JLabel("Alta Paciente");
+		lblNewLabel.setFont(new Font("Monospaced", Font.BOLD, 40));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+		JLabel lblMensajeDelSistema = new JLabel("Mensaje del sistema");
+		lblMensajeDelSistema.setForeground(Color.RED);
+		lblMensajeDelSistema.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMensajeDelSistema.setFont(new Font("Monospaced", Font.BOLD, letraPequena));
+
+		JButton btnNewButton = new JButton("Aceptar");
+		btnNewButton.setFont(new Font("Monospaced", Font.BOLD, letraPequena));
+
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup().addGap(20)
+						.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 958, Short.MAX_VALUE).addGap(20))
+				.addGroup(groupLayout.createSequentialGroup().addGap(100).addGroup(groupLayout
+						.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblMensajeDelSistema, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 798,
+								Short.MAX_VALUE)
+						.addGroup(groupLayout.createSequentialGroup()
+								.addComponent(panelLabel, GroupLayout.PREFERRED_SIZE, 231, GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 164,
+												GroupLayout.PREFERRED_SIZE)
+										.addComponent(panelText, GroupLayout.DEFAULT_SIZE, 561, Short.MAX_VALUE))))
+						.addGap(100)));
+		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup().addGap(20)
+						.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
+						.addGap(30)
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(panelLabel, GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+								.addComponent(panelText, GroupLayout.DEFAULT_SIZE, 365, Short.MAX_VALUE))
+						.addGap(18).addComponent(btnNewButton).addGap(25)
+						.addComponent(lblMensajeDelSistema, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+						.addGap(43)));
+		panelLabel.setBackground(Color.WHITE);
+		panelText.setBackground(Color.WHITE);
+		panelText.setLayout(new GridLayout(1, 0, 0, 0));
+		panelLabel.setLayout(new GridLayout(1, 0, 0, 0));
+		setLayout(groupLayout);
+		creandoVista();
+		setVisible(true);
 	}
 
+	private void creandoVista() {
+		Color color = new Color(133, 200, 255);
+		this.panelLabel.setBackground(color);
+		this.panelText.setBackground(color);
+		setBackground(color);
+		this.fields = new JTextField[this.label.length];
+		panelText.setLayout(new GridLayout(this.label.length, 1, 30, 50));
+		panelLabel.setLayout(new GridLayout(this.label.length, 1, 10, 50));
+		for (int i = 0; i < fields.length; i++) {
+			this.fields[i] = crearJText();
+			this.panelLabel.add(crearLabel(this.label[i]));
+			this.panelText.add(this.fields[i]);
+		}
+		revalidate();
+	}
+
+	private JTextField crearJText() {
+		JTextField field = new JTextField();
+		field.setText("");
+		field.setFont(new Font("Monospaced", Font.BOLD, letraPequena));
+		field.setHorizontalAlignment(SwingConstants.CENTER);
+		field.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		return field;
+	}
+
+	private JLabel crearLabel(String cadena) {
+		JLabel jLabel = new JLabel();
+		jLabel.setText(cadena);
+		jLabel.setFont(new Font("Monospaced", Font.BOLD, letraPequena));
+		jLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		return jLabel;
+	}
 }
