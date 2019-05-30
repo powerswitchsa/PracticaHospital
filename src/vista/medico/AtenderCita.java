39c0fce261df9b301e7864b812d5f7fef3f6d2eb
Manu <Manu@GloriaPC>
2019-05-30 18:42:59 +0200

    Atendercita

--- src/vista/medico/AtenderCita.java
+++ src/vista/medico/AtenderCita.java
@@ -30,18 +30,18 @@ public class AtenderCita extends JPanel {
 	public AtenderCita(Color color, int letraPequena, int letraGrande, String tipoLetra) {
 		setBackground(color);
 
-		setBorder(new LineBorder(new Color(0, 0, 0)));
+		setBorder(null);
 
 		JLabel lblMedico = new JLabel("Medico");
-		lblMedico.setFont(new Font(tipoLetra, Font.BOLD, 15));
+		lblMedico.setFont(new Font(tipoLetra, Font.BOLD, 20));
 		lblMedico.setHorizontalAlignment(SwingConstants.CENTER);
 
 		JLabel lblPaciente = new JLabel("Paciente");
-		lblPaciente.setFont(new Font(tipoLetra, Font.BOLD, 15));
+		lblPaciente.setFont(new Font(tipoLetra, Font.BOLD, 20));
 		lblPaciente.setHorizontalAlignment(SwingConstants.CENTER);
 
 		JLabel lblHoraCita = new JLabel("Hora cita");
-		lblHoraCita.setFont(new Font(tipoLetra, Font.BOLD, 15));
+		lblHoraCita.setFont(new Font(tipoLetra, Font.BOLD, 20));
 		lblHoraCita.setHorizontalAlignment(SwingConstants.CENTER);
 
 		JLabel lblTitulo = new JLabel("Atencion a cita");
@@ -56,103 +56,99 @@ public class AtenderCita extends JPanel {
 		txtHoraCita.setColumns(10);
 
 		JLabel lblObservaciones = new JLabel("Observaciones");
-		lblObservaciones.setFont(new Font(tipoLetra, Font.BOLD, 15));
+		lblObservaciones.setFont(new Font(tipoLetra, Font.BOLD, 20));
 
 		txtObservaciones = new JTextField();
 		txtObservaciones.setColumns(10);
 
 		JButton btnAplicar = new JButton("Aplicar");
 
-		JLabel lblMensaje = new JLabel("Mensaje");
-		lblMensaje.setFont(new Font(tipoLetra, Font.BOLD, 15));
-
 		txtMensaje = new JTextField();
+		txtMensaje.setBorder(null);
+		txtMensaje.setFont(new Font(tipoLetra, Font.BOLD, 20));
+		txtMensaje.setForeground(color.RED);
+		txtMensaje.setEditable(false);
+		txtMensaje.setText("Mensaje del Sistema");
+		txtMensaje.setOpaque(false);
 		txtMensaje.setFocusable(false);
 		txtMensaje.setColumns(10);
 
 		JPanel panel = new JPanel();
+		panel.setOpaque(false);
 		GroupLayout groupLayout = new GroupLayout(this);
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(24)
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-							.addComponent(lblMedico, GroupLayout.PREFERRED_SIZE, 102, GroupLayout.PREFERRED_SIZE)
-							.addComponent(lblPaciente, GroupLayout.PREFERRED_SIZE, 102, GroupLayout.PREFERRED_SIZE))
-						.addComponent(lblHoraCita, GroupLayout.PREFERRED_SIZE, 102, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(lblObservaciones)
-							.addContainerGap())
-						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-							.addGroup(groupLayout.createSequentialGroup()
-								.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-									.addComponent(lblTitulo, GroupLayout.PREFERRED_SIZE, 522, GroupLayout.PREFERRED_SIZE)
-									.addGroup(groupLayout.createSequentialGroup()
-										.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-											.addGroup(groupLayout.createSequentialGroup()
-												.addComponent(lblMensaje, GroupLayout.PREFERRED_SIZE, 114, GroupLayout.PREFERRED_SIZE)
-												.addPreferredGap(ComponentPlacement.UNRELATED)
-												.addComponent(txtMensaje, GroupLayout.DEFAULT_SIZE, 297, Short.MAX_VALUE)
-												.addGap(109)
-												.addComponent(btnAplicar, GroupLayout.PREFERRED_SIZE, 102, GroupLayout.PREFERRED_SIZE))
-											.addGroup(groupLayout.createSequentialGroup()
-												.addComponent(txtObservaciones, GroupLayout.DEFAULT_SIZE, 132, Short.MAX_VALUE)
-												.addPreferredGap(ComponentPlacement.RELATED)
-												.addComponent(panel, GroupLayout.DEFAULT_SIZE, 395, Short.MAX_VALUE)))
-										.addGap(30)))
-								.addGap(5))
-							.addGroup(groupLayout.createSequentialGroup()
-								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-									.addComponent(txtPaciente, GroupLayout.DEFAULT_SIZE, 376, Short.MAX_VALUE)
+							.addGap(20)
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addComponent(lblPaciente, GroupLayout.PREFERRED_SIZE, 102, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblMedico, GroupLayout.PREFERRED_SIZE, 102, GroupLayout.PREFERRED_SIZE)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(lblHoraCita, GroupLayout.PREFERRED_SIZE, 111, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED)))
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(10)
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addComponent(lblObservaciones)
+										.addGroup(groupLayout.createSequentialGroup()
+											.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+												.addComponent(txtObservaciones, GroupLayout.DEFAULT_SIZE, 273, Short.MAX_VALUE)
+												.addComponent(txtMensaje, GroupLayout.DEFAULT_SIZE, 273, Short.MAX_VALUE))
+											.addPreferredGap(ComponentPlacement.RELATED)
+											.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+												.addComponent(panel, GroupLayout.DEFAULT_SIZE, 274, Short.MAX_VALUE)
+												.addComponent(btnAplicar, GroupLayout.PREFERRED_SIZE, 102, GroupLayout.PREFERRED_SIZE)))))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(18)
 									.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
-										.addComponent(comboBoxMedico, Alignment.LEADING, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-										.addComponent(txtHoraCita, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 236, Short.MAX_VALUE)))
-								.addContainerGap(192, Short.MAX_VALUE)))))
+										.addComponent(txtHoraCita, Alignment.LEADING)
+										.addComponent(txtPaciente, Alignment.LEADING)
+										.addComponent(comboBoxMedico, Alignment.LEADING, 0, 221, Short.MAX_VALUE)))))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(67)
+							.addComponent(lblTitulo, GroupLayout.PREFERRED_SIZE, 525, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(22)
-							.addComponent(lblTitulo, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
-							.addGap(128))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(60)
-							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblMedico, GroupLayout.DEFAULT_SIZE, 44, Short.MAX_VALUE)
-								.addComponent(comboBoxMedico, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblPaciente, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
-								.addComponent(txtPaciente, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-								.addComponent(txtHoraCita, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblHoraCita, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE))
-							.addPreferredGap(ComponentPlacement.RELATED)))
+					.addContainerGap()
+					.addComponent(lblTitulo, GroupLayout.PREFERRED_SIZE, 37, GroupLayout.PREFERRED_SIZE)
+					.addGap(30)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblMedico)
+						.addComponent(comboBoxMedico, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblPaciente, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtPaciente, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblHoraCita, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtHoraCita, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(lblObservaciones, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
 					.addGap(6)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(panel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtObservaciones, GroupLayout.PREFERRED_SIZE, 193, GroupLayout.PREFERRED_SIZE))
-					.addGap(35)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnAplicar)
-						.addComponent(txtMensaje, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblMensaje))
-					.addGap(43))
+						.addComponent(panel, GroupLayout.DEFAULT_SIZE, 208, Short.MAX_VALUE)
+						.addComponent(txtObservaciones, GroupLayout.DEFAULT_SIZE, 208, Short.MAX_VALUE))
+					.addGap(13)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(txtMensaje, GroupLayout.DEFAULT_SIZE, 59, Short.MAX_VALUE)
+						.addComponent(btnAplicar))
+					.addContainerGap())
 		);
 
 		JLabel lblNewLabel_2 = new JLabel("Medicamento");
+		lblNewLabel_2.setFont(new Font(tipoLetra, Font.BOLD, 20));
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 
-		JLabel label = new JLabel("Dosis");
-		label.setHorizontalAlignment(SwingConstants.CENTER);
+		JLabel labelDosis = new JLabel("Dosis");
+		labelDosis.setFont(new Font(tipoLetra, Font.BOLD, 20));
+		labelDosis.setHorizontalAlignment(SwingConstants.CENTER);
 
 		JComboBox comboBoxMedicamento = new JComboBox();
 
@@ -160,12 +156,14 @@ public class AtenderCita extends JPanel {
 		txtPeriodo.setColumns(10);
 
 		JLabel lblPeriodo = new JLabel("Periodo");
+		lblPeriodo.setFont(new Font(tipoLetra, Font.BOLD, 20));
 		lblPeriodo.setHorizontalAlignment(SwingConstants.CENTER);
 
 		txtDosis = new JTextField();
 		txtDosis.setColumns(10);
 
 		JLabel lblTratamiento = new JLabel("Tratamiento");
+		lblTratamiento.setFont(new Font(tipoLetra, Font.BOLD, 20));
 		lblTratamiento.setHorizontalAlignment(SwingConstants.CENTER);
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(gl_panel.createParallelGroup(Alignment.LEADING).addGroup(gl_panel
@@ -178,7 +176,7 @@ public class AtenderCita extends JPanel {
 				.addGroup(gl_panel.createSequentialGroup().addGap(20)
 						.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
 								.addComponent(lblPeriodo, GroupLayout.PREFERRED_SIZE, 137, GroupLayout.PREFERRED_SIZE)
-								.addComponent(label, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE))
+								.addComponent(labelDosis, GroupLayout.PREFERRED_SIZE, 126, GroupLayout.PREFERRED_SIZE))
 						.addGap(35)
 						.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
 								.addComponent(txtDosis, GroupLayout.DEFAULT_SIZE, 167, Short.MAX_VALUE)
@@ -191,7 +189,7 @@ public class AtenderCita extends JPanel {
 								.addComponent(comboBoxMedicamento, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
 										GroupLayout.PREFERRED_SIZE))
 						.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_panel.createSequentialGroup().addGap(23).addComponent(label))
+								.addGroup(gl_panel.createSequentialGroup().addGap(23).addComponent(labelDosis))
 								.addGroup(gl_panel.createSequentialGroup().addGap(18).addComponent(txtDosis,
 										GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
 										GroupLayout.PREFERRED_SIZE)))
