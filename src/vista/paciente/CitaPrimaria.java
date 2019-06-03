4537f13588b2f3334accabce2d16ae74fb738d70
hh3 <Ho33@users.noreply.github.com>
2019-06-03 20:58:11 +0200

    iniciocitaprimaria

--- src/vista/paciente/CitaPrimaria.java
+++ src/vista/paciente/CitaPrimaria.java
@@ -1,5 +1,27 @@
 package vista.paciente;
 
-public class CitaPrimaria {
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+
+public class CitaPrimaria extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public CitaPrimaria() {
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 550, Short.MAX_VALUE)
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 414, Short.MAX_VALUE)
+		);
+		setLayout(groupLayout);
+
+	}
 
 }
