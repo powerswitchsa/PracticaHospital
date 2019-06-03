ee649bd5d3549f54c64c74d4a183eec81ffefd80
hh3 <Ho33@users.noreply.github.com>
2019-06-03 18:26:10 +0200

    almacen medico inicio

--- src/almacen/AlmacenMedico.java
+++ src/almacen/AlmacenMedico.java
@@ -1,18 +1,31 @@
 package almacen;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import modelo.Medico;
 
 public class AlmacenMedico {
 
-	private DAO<ArrayList<Medico>> DAO;
-	private ArrayList<Medico> medicos;
+	private DAO<HashMap<String, Medico>> DAO;
+	private HashMap<String, Medico> mapMedicos;
 	private String rutaDestino = "./resource/Medicos/medicos.dat";
 
 	public AlmacenMedico() {
 		super();
-
+		this.DAO = new DAO<HashMap<String, Medico>>();
+		File file = new File(rutaDestino);
+		if (!file.exists()) {
+			file.mkdir();
+		}
 	}
-
+	public boolean bajaMedico(String id) {
+		this.mapMedicos.remove(id);
+		return false;
+		
+	}
+	private boolean grabarMapMedico() {
+		return this.DAO.grabar(rutaDestino, this.mapMedicos);
+	}
 }
