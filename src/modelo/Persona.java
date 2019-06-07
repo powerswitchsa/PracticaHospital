7a4a4051aef556701deacb433c4e4312a631cb06
Jesus <jesusfondon@gmail.com>
2019-06-07 12:37:01 +0200

    Validadores y su test, clase Controller y Persona

--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -3,6 +3,8 @@ package modelo;
 import java.io.Serializable;
 import java.util.regex.Pattern;
 
+import modelo.enums.Regularizador;
+
 public abstract class Persona implements Serializable {
 
 	private String id;
@@ -95,32 +97,33 @@ public abstract class Persona implements Serializable {
 	// validadores
 
 	public static Respuesta validarNombre(String nombre) {
-		boolean resultado = Pattern.matches("^([A-Z]{1}[a-z]+[ ]?){1,2}$*", nombre);
+		boolean resultado = Pattern.matches(Regularizador.Nombre.getCadena(), nombre);
 		return new Respuesta(resultado, (resultado? "nombre valido" : "nombre invalido"));
 	}
 
 	public static Respuesta validarApellido(String apellido) {
-		return new Respuesta(Pattern.matches("^([A-Z]{1}[a-z]+[ ]?){1,2}$*", apellido), "");
+		boolean resultado = Pattern.matches(Regularizador.Apellido.getCadena(), apellido);
+		return new Respuesta(resultado, (resultado? "apellido valido" : "apellido invalido"));
 	}
 
-	// que no tenga numero el nombre de la calle, que empieze por C/, al final ponga un numero
 	public static Respuesta validarDireccion(String direccion) {
-		boolean resultado = Pattern.matches("C/[ ]([A-Z]{1}[a-z]+[ ]?){1,}$*[0-9]{1,}", direccion);
+		boolean resultado = Pattern.matches(Regularizador.Direccion.getCadena(), direccion);
 		return new Respuesta(resultado, (resultado? "direccion correcta" : "direccion incorrecta"));
 	}
 
 	public static Respuesta validarTelefono(String telefono) {
-		return new Respuesta(Pattern.matches("[6-7-9]\\d{8}", telefono), "Es valido el telefono de contacto");
+		boolean resultado = Pattern.matches(Regularizador.Telefono.getCadena(), telefono);
+		return new Respuesta(resultado, (resultado? "telefono correcto" : "telefono incorrecto"));
 	}
 
 	public static Respuesta validarNacimiento(String nacimiento) {
-		boolean resultado = Pattern.matches("[0-9]{1,2}(/|-)[0-9]{1,2}(/|-)[0-9]{4}", nacimiento);
+		boolean resultado = Pattern.matches(Regularizador.Nacimiento.getCadena(), nacimiento);
 		return new Respuesta(resultado, (resultado? "fecha correcta" : "fecha incorrecta")) ;
 	}
 
 	public static Respuesta validarId(String id) {
-		boolean resultado = Pattern.matches("[0-9]{1,}", id);
-		return new Respuesta(resultado, (resultado? "ID correcta" : "ID incorrecta"));
+		boolean resultado = Pattern.matches(Regularizador.Id.getCadena(), id);
+		return new Respuesta(resultado, (resultado? "ID correcto" : "ID incorrecto"));
 
 	}
 	
---
+++ src/modelo/enums/Regularizador.java
@@ -0,0 +1,20 @@
+package modelo.enums;
+
+public enum Regularizador {
+
+	Nombre("^([A-Z]{1}[a-z]+[ ]?){1,2}$*"), Apellido("^([A-Z]{1}[a-z]+[ ]?){1,2}$*"), 
+	Telefono("[6-7-9]\\d{8}"), 
+	Nacimiento("^\\s*(3[01]|[12][0-9]|0?[1-9])\\/(1[012]|0?[1-9])\\/((?:19|20)\\d{2})\\s*$"),
+	Id("[0-9]{1,}"), Direccion("C/[ ]([A-Z]{1}[a-z]+[ ]?){1,}$*[0-9]{1,}(º)");
+
+	private String cadena;
+
+	private Regularizador(String cadena) {
+		this.cadena = cadena;
+	}
+
+	public String getCadena() {
+		return cadena;
+	}
+	
+}
